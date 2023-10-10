58ce12da0549bd971e912b37e7efa6883f37b1c7
Priscila Otaro <priscilaotaro.g26@gmail.com>
2023-10-10 16:08:48 -0300

    Implementación de busqueda de tareas

--- app/Http/Controllers/TareasController.php
+++ app/Http/Controllers/TareasController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tareas;
 use Illuminate\Http\Request;
+use Illumiate\Database\Eloquent\ModelNotFoundException;
+
 
 class TareasController extends Controller
 {
-    //
+    public function buscarPorTitulo ($titulo) {
+        try {
+            $tituloDeTarea = Tareas::where('titulo', $titulo)->get();
+            return $tituloDeTarea;
+        } 
+    
+        catch (ModelNotFoundException $exception) {
+            return response()->json(['error' => 'Tareas no encontradas'], 404);
+        }
+    }
+    
+    public function buscarPorAutor ($autor) {
+        try {
+            $autorDeTarea = Tareas::where('autor', $autor)->get();
+            return $autorDeTarea;
+        } 
+    
+        catch (ModelNotFoundException $exception) {
+            return response()->json(['error' => 'Tareas no encontradas'], 404);
+        }
+    }
+
+    public function buscarPorEstado ($estado) {
+        try {
+            $estadoDeTarea = Tareas::where('estado', $estado)->get();
+            return $estadoDeTarea;
+        } 
+    
+        catch (ModelNotFoundException $exception) {
+            return response()->json(['error' => 'Tareas no encontradas'], 404);
+        }
+    }
+
 }
+
