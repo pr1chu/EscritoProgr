290c9bf8a9c797a4b523d28a427aba4a20d49243
Priscila Otaro <priscilaotaro.g26@gmail.com>
2023-10-10 16:42:17 -0300

    Relacionando Models con la base de datos

--- app/Models/Tareas.php
+++ app/Models/Tareas.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Tareas extends Model
 {
     use HasFactory;
+    protected $table = 'tareas';
+    protected $primarykey = 'id';
+    public $timestamps = true;
+    protected $fillable = [
+        'titulo',
+        'contenido',
+        'estado',
+        'autor',
+    ] 
 }
