22b2df4a0919fb7adb8ffa6870991d105937cb00
Priscila Otaro <priscilaotaro.g26@gmail.com>
2023-10-10 16:18:05 -0300

    añadido de Routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/{id}', [TareasController::class, 'Buscar']);
+
+Route::get('/{titulo}', [TareasController::class, 'buscarPorTitulo']);
+
+Route::get('/{autor}', [TareasController::class, 'buscarPorAutor']);
+
+Route::get('/{estado}', [TareasController::class, 'buscarPorEstado']);
+
+
